92d2ed36aa53fa95493d3f7ea9107e96a1a7d5cf
pratish <psnanandkar@gmail.com>
2021-10-04 10:40:20 +0530

    "Third Push"

--- src/main/java/First.java
+++ src/main/java/First.java
@@ -2,5 +2,10 @@ public class First {
 
     public static  void main(String[] args){
         System.out.println("Hello to the GitHub from Intellij");
+
+        // Now, I am adding the comment to see how this works there!!
+        /*
+        *   IntelliJ is working fine with the GitHub!!
+        * */
     }
 }
